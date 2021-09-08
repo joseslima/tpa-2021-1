5cd1f7bd42efb895fdc63d3805bf53cb01ae3300
joseslima <guislima47@gmail.com>
2021-09-08 15:21:41 -0300

    10131-veredito

--- 10131/10131.py
+++ 10131/10131.py
@@ -1,40 +1,40 @@
-dp = []
+cache = []
 elems = []
 
-def percorrer(pos):
-	global dp,elems
+def walk(pos):
+	global cache,elems
 	id = elems[pos][2]
-	if (dp[id] != -1):
-		return dp[id]
+	if (cache[id] != -1):
+		return cache[id]
 
-	tam = 1
+	size = 1
 	for j in range(pos+1,len(elems)):
 		if elems[j][0] > elems[pos][0] and elems[j][1] < elems[pos][1]:
-			tam = max(tam, 1 + percorrer(j))
-	dp[id] = tam	
-	return dp[id]
+			size = max(size, 1 + walk(j))
+	cache[id] = size	
+	return cache[id]
 
-def pega_maior(pos,maior,n):
+def getBigger(pos,bigger,n):
 	id = elems[pos][2]
 	print(id)
 	for i in range(pos+1,n):
 		id = elems[i][2]
-		if maior == dp[id]:
-			pega_maior(i,maior-1,n)
+		if bigger == cache[id]:
+			getBigger(i,bigger-1,n)
 			return
 
 def main():
-	global dp,elems
+	global cache,elems
 	while True:
 		try:
 			id = 1
-			entrada = input().split()
-			while len(entrada) == 2:
-				entrada[0] = int(entrada[0])
-				entrada[1] = int(entrada[1])
-				entrada.append(id)
-				elems.append(entrada)
-				entrada = input().split()
+			inpu = input().split()
+			while len(inpu) == 2:
+				inpu[0] = int(inpu[0])
+				inpu[1] = int(inpu[1])
+				inpu.append(id)
+				elems.append(inpu)
+				inpu = input().split()
 				id+=1
 
 		except EOFError:
@@ -42,18 +42,18 @@ def main():
 			n = len(elems)
 			if n == 0:
 				return 0
-			dp = [-1] * (n+1)
-			maior = 0
+			cache = [-1] * (n+1)
+			bigger = 0
 			for i in range(n):
-				tam = percorrer(i)
-				if tam > maior:
-					maior = tam
+				size = walk(i)
+				if size > bigger:
+					bigger = size
 
-			print(maior)
+			print(bigger)
 			for i in range(0,n):
 				id = elems[i][2]
-				if maior == dp[id]:
-					pega_maior(i,maior-1,n)
+				if bigger == cache[id]:
+					getBigger(i,bigger-1,n)
 					return 0
 			break
 	return 0
