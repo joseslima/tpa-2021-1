50f9d3e018e8cb81e9f581b07cc3d1efdaf36cfb
joseslima <guislima47@gmail.com>
2021-09-08 21:08:04 -0300

    12190

---
+++ 12190/12190.py
@@ -0,0 +1,81 @@
+consumptionRange  = [ 100, 9900, 990000 ]
+prices = [2, 3, 5, 7]
+
+dp = {}
+
+def getTEnergy(price):
+	global consumptionRange, prices
+	
+	tEnergy = 0
+
+	if (price in dp):
+		return dp[price]
+
+	if (price == 0):
+		return tEnergy
+
+	for i in range(len(consumptionRange)):
+		tEnergy += min( max(0 , price / prices[i]), consumptionRange[i])
+		price -= prices[i]*consumptionRange[i]
+	
+	tEnergy += max(0, price/prices[3])
+	
+	dp[price] = tEnergy
+
+	return tEnergy
+
+def cost(energy):
+	global consumptionRange, prices
+
+	price = 0
+
+	if (energy == 0):
+		return price
+
+	for i in range(len(consumptionRange)):
+		price += min(max(0, energy*prices[i]), prices[i]*consumptionRange[i]) 
+		energy -= consumptionRange[i]
+
+	price += max(0, energy*prices[3])
+
+	return price
+
+def main():
+	newLine = input()
+	while ( newLine ) :
+		splitedInput = newLine.split()
+		
+		together_price = int(splitedInput[0])
+		
+		difference_price = int(splitedInput[1])
+		
+		if (not together_price or  not difference_price):
+			break
+			
+		tEnergy = getTEnergy(together_price)
+		start = 0
+		end = tEnergy
+
+		aux = 1
+
+		while (start < end and aux):
+			m = (start + end) // 2
+			
+			a = cost(tEnergy - m)
+			b = cost(m)
+			
+			diff = a - b;
+			
+			if ( diff > difference_price):
+				start = m
+			
+			else:
+				if (diff  < difference_price):
+					end = m
+				else:
+					print(int(cost(m)))
+					aux = 0
+		newLine = input()
+	return 0
+	
+main()
---
+++ 12190/README.md
@@ -0,0 +1,35 @@
+# Trabalho de Programação
+
+**Dividir e Conquistar & Programação Dinâmica & Algoritmos Gulosos**
+
+**Autor:** José Guilherme Silva de Lima
+
+**Data:** 08 de setembro de 2021
+
+**Problema:** 12190	- Is Bigger Smarter?
+
+## Sobre a Solução
+Este diretório contém o código fonte gerado para solucionar o problema 12190
+do *Online Judge*. 
+
+para resolver esse problema foi utilizada a estratégiaa de algoritmo guloso nas iterações que são realizadas
+nos métodos `getTEnergy` e `cost`, e também a utilização do conceito de programação dinamica armazenando um 
+cache para evitar o recalculo em `getTEnergy`.
+
+
+O problema recebeu veredito “Accepted”, como mostrado na
+figura abaixo:
+
+![Veredito](./12190-veredito.png)
+
+O programa foi desenvolvido em Python3. Para compilar o programa deve ser usado
+o seguinte comando:
+```
+python3 12190.py
+```
+caso deseje utilizar algum arquivo txt como input, execute o seguinte comando:
+```
+python3 12190.py < input.txt
+```
+
+
